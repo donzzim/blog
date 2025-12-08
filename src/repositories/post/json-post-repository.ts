575d25ebb5b7681897549d817c14391a7319f8ff
Lucas César <lucascesarmengo@gmail.com>
2025-12-08 18:20:58 -0300

    posts update

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,10 +1,40 @@
 import { PostModel } from "@/src/models/post/post-model";
 import { PostRepository } from "./post-repository";
+import { resolve } from "path";
+import { readFile } from "fs/promises";
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(ROOT_DIR, 'db', 'seed', 'posts.json');
 
 export class JsonPostRepository implements PostRepository {
-    async findAll(): Promise<PostModel[]> { }
+    private async readFromDisk(): Promise<PostModel[]> {
+        const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
+        const parsedJson = JSON.parse(jsonContent);
+        const { posts } = parsedJson;
+        return posts;
+    }
+    async findAll(): Promise<PostModel[]> {
+        const posts = this.readFromDisk();
+        return posts;
+    }
+
+    async findById(id: string): Promise<PostModel> {
+        const posts = await this.findAll();
+        const post = posts.find(post => post.id === id);
+
+        if (!post) {
+            throw new Error(`Post with id ${id} not found`);
+        }
+
+        return post;
+    }
 }
 
-export const PostRepository = new JsonPostRepository();
+export const postRepository: PostRepository = new JsonPostRepository();
+
+// (async () => {
+//     const posts = await postRepository.findAll();
+//     posts.forEach(post => {
+//         console.log(`Post: ${post.title} by ${post.author}`);
+//     });
+// })();
--- src/repositories/post/post-repository.ts
+++ src/repositories/post/post-repository.ts
@@ -2,4 +2,5 @@ import { PostModel } from "@/src/models/post/post-model";
 
 export interface PostRepository {
     findAll(): Promise<PostModel[]>;
+    findById(id: string): Promise<PostModel | undefined>;
 };
